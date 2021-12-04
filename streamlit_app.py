dbc1786b441dc9e79ea714641c8d00bb1718dcb8
luoxu128 <luoxu128@gmail.com>
2021-12-04 21:46:51 +0800

    第三次更新

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,22 +55,65 @@ def main():
         if submit:
             st.write(f'欢迎 {name}回来')
     
-    d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
-    st.write('Your birthday is:', d)
-    
-    #t = st.time_input('Set an alarm for', datetime.time(8, 45))
-    st.write('Alarm is set for', t)
+        d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+        st.write('Your birthday is:', d)
+        
+        #t = st.time_input('Set an alarm for', datetime.time(8, 45))
+        st.write('Alarm is set for', t)
     
     if page2:
         st.info("请欣赏雪山景色")
-        image = Image.open('./图片/图片 (5).jpg')
-        st.image(image, use_column_width='auto', caption='雪山景色',output_format="png")
+        #转换时间为时间戳，加8小时时差，再转换为时间元组
+        ep = datetime.datetime(1970, 1, 1)
+        dt = datetime.datetime.now()
+        dt = (dt - ep )/datetime.timedelta(seconds=1) + 3600*8
+        localtime = time.gmtime(dt)
+        #localtime = time.localtime(time.time())
+        #print "本地时间为 :", localtime
+        #localtime = time.asctime(time.localtime(time.time()))
+        hour = localtime.tm_hour
+        minute = localtime.tm_min
+        hour = st.slider('hour', 0, 23, hour)
+        minute = st.slider('minute', 0, 59, minute)
+        st.write('Time is', hour, ':', minute)
+        st.write(f'当前时间为 {t}')
+        
+        tic1 = time.perf_counter()
+        #time.sleep(2)#(此行可以换成需要计时的模块)
+        toc1 = time.perf_counter()
+        shijian1 = int(toc1-tic1)
+        st.write(shijian1)
+        
+        if st.button('Say hello'):
+             st.write('Why hello there')
+        else:
+             st.write('Goodbye')
+
+
     if page3:
         st.info('点击播放按钮播放mp3音乐')
-        audio_file = open('./视频与图像/Welcome.mp3', 'rb')
-        audio_bytes = audio_file.read()
-        st.audio(audio_bytes, format='audio/mp3')
+        with st.echo(code_location='below'):
+            total_points = st.slider("Number of points in spiral", 1, 5000, 3000)
+            num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+        
+            Point = namedtuple('Point', 'x y')
+            data = []
+        
+            points_per_turn = total_points / num_turns
+        
+            for curr_point_num in range(total_points):
+                curr_turn, i = divmod(curr_point_num, points_per_turn)
+                angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+                radius = curr_point_num / total_points
+                x = radius * math.cos(angle)
+                y = radius * math.sin(angle)
+                data.append(Point(x, y))
+        
+            st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+                .mark_circle(color='#0068c9', opacity=0.5)
+                .encode(x='x:Q', y='y:Q'))
     
+
     st.markdown("""
     <style>
     div.stButton > button:first-child {
@@ -80,31 +123,6 @@ def main():
 
 
     
-    #转换时间为时间戳，加8小时时差，再转换为时间元组
-    ep = datetime.datetime(1970, 1, 1)
-    dt = datetime.datetime.now()
-    dt = (dt - ep )/datetime.timedelta(seconds=1) + 3600*8
-    localtime = time.gmtime(dt)
-    #localtime = time.localtime(time.time())
-    #print "本地时间为 :", localtime
-    #localtime = time.asctime(time.localtime(time.time()))
-    hour = localtime.tm_hour
-    minute = localtime.tm_min
-    hour = st.slider('hour', 0, 23, hour)
-    minute = st.slider('minute', 0, 59, minute)
-    st.write('Time is', hour, ':', minute)
-    st.write(f'当前时间为 {t}')
-    
-    tic1 = time.perf_counter()
-    #time.sleep(2)#(此行可以换成需要计时的模块)
-    toc1 = time.perf_counter()
-    shijian1 = int(toc1-tic1)
-    st.write(shijian1)
-    
-    if st.button('Say hello'):
-         st.write('Why hello there')
-    else:
-         st.write('Goodbye')
     
     
     """
@@ -119,26 +137,6 @@ def main():
     """
     
     
-    with st.echo(code_location='below'):
-        total_points = st.slider("Number of points in spiral", 1, 5000, 3000)
-        num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-    
-        Point = namedtuple('Point', 'x y')
-        data = []
-    
-        points_per_turn = total_points / num_turns
-    
-        for curr_point_num in range(total_points):
-            curr_turn, i = divmod(curr_point_num, points_per_turn)
-            angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-            radius = curr_point_num / total_points
-            x = radius * math.cos(angle)
-            y = radius * math.sin(angle)
-            data.append(Point(x, y))
-    
-        st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-            .mark_circle(color='#0068c9', opacity=0.5)
-            .encode(x='x:Q', y='y:Q'))
 
 if __name__ == '__main__':
     main()
