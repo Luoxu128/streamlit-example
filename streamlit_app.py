b18409477e699377ded333f93b17b6bf94c37d3b
luoxu128 <luoxu128@gmail.com>
2021-12-05 17:24:07 +0800

    横向菜单方案一测试

--- streamlit_app.py
+++ streamlit_app.py
@@ -117,21 +117,13 @@ def main():
     #横向菜单方案一结束    
     
 
-    left, col1, left_medium, col2, right_medium, col3, right = st.columns([0.1,1,0.1,1,0.1,1,0.1])
-    with left:
-         st.empty()
+   col1, col2, col3 = st.columns(3)
     with col1:
         page1 = st.button("视频")
-    with left_medium:
-        st.empty()
     with col2:
         page2 = st.button("图片")
-    with right_medium:
-        st.empty()
     with col3:
         page3 = st.button("音乐")
-    with right:
-        st.empty()
         
     if page1:
         st.session_state.page = 1
