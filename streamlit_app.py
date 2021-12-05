9b28606e658fdcb94324fdb19ddb1a56090a96ce
luoxu128 <luoxu128@gmail.com>
2021-12-05 20:58:56 +0800

    横向菜单方案一测试

--- streamlit_app.py
+++ streamlit_app.py
@@ -55,10 +55,13 @@ def main():
         unsafe_allow_html=True,
         )
 
+    #测试tab
+    
+    active_tab = tabs(["Tab 1", "Tab 2", "Tab 3"])
+    st.write(active_tab)
+
 
     #横向菜单方案一
-    
-
     test_html = f"""
         <html>
                 <div id="test_div">
@@ -243,7 +246,35 @@ def main():
     In the meantime, below is an example of what you can do with just a few lines of code:
     """
     
-    
+def tabs(default_tabs = [], default_active_tab=0):
+        if not default_tabs:
+            return None
+        active_tab = st.radio("", default_tabs, index=default_active_tab)
+        child = default_tabs.index(active_tab)+1
+        st.markdown("""  
+            <style type="text/css">
+            div[role=radiogroup] > label > div:first-of-type {
+               display: none
+            }
+            div[role=radiogroup] {
+                flex-direction: unset
+            }
+            div[role=radiogroup] label {             
+                border: 1px solid #999;
+                background: #EEE;
+                padding: 4px 12px;
+                border-radius: 4px 4px 0 0;
+                position: relative;
+                top: 1px;
+                }
+            div[role=radiogroup] label:nth-child(""" + str(child) + """) {    
+                background: #FFF !important;
+                border-bottom: 1px solid transparent;
+            }            
+            </style>
+        """,unsafe_allow_html=True)        
+        return active_tab
+   
 
 if __name__ == '__main__':
     main()
